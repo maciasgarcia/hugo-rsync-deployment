205052f23dbebac462f6287512fe1ea372e520a5
Juan Antonio Macías <juamacgar@gmail.com>
2023-02-02 14:30:07 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [[ "${BUILD_MODE}" == 'hugo' ]]; then
 fi
 
 if [[ "${BUILD_MODE}" == 'doks' ]]; then
-  curl -fsSL https://deb.nodesource.com/setup_16.x | bash -c "apt install -y nodejs"
+  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -c "apt install -y nodejs"
   npm install
   npm run build
 fi
